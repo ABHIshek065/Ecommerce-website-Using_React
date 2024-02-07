fc738ba30b96a685610cbaf9a9ba89569bb8c99c
Abhishek Patel <98528020+Abhi8755@users.noreply.github.com>
2024-02-07 21:10:00 +0530

    related products page added

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+    return (
+        <div className='discriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (133)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>
+                    Dive into a world of endless style at ShopperStops, your one-stop shop for curated fashion online. Explore our diverse collections for men, women, and kids, featuring everything from trendy threads to timeless essentials. Browse vibrant dresses,  kids, statement shirts, and comfy jeans, all handpicked to flatter every figure and embrace every personality. Discover hidden gems from independent designers alongside renowned brands, ensuring you stay ahead of the curve. </p>
+                <p>
+                    Clear, detailed size guides with accurate measurements & fit advice. Offer multiple size systems & conversion charts. Include visuals & try-on guides. Consider fit quizzes & user reviews for real-world insights. Aim for transparency & build trust for fewer returns & happier customers! </p>
+            </div>
+        </div>
+    )
+}
